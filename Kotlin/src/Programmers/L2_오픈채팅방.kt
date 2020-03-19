d39448eb4882db2a60297811bd4000a579d9b4d2
IndongLee <kundera@kakao.com>
2020-03-20 00:32:44 +0900

    Solve 오픈채팅방

---
+++ Kotlin/src/Programmers/L2_오픈채팅방.kt
@@ -0,0 +1,16 @@
+package Programmers
+
+fun solution(record: Array<String>): Array<String> {
+    val userMap = hashMapOf<String, String>()
+    val command = mapOf("Enter" to "들어왔습니다.", "Leave" to "나갔습니다.")
+    return record.map {
+            val rec = it.split(" ")
+            if (rec.size == 3) userMap[rec[1]] = rec[2]
+            Pair(rec[1], rec[0])
+        }.asSequence().filter { it.second[0] != 'C' }
+        .map { "\"${userMap[it.first]}님이 ${command[it.second]}\"" }.toList().toTypedArray()
+}
+
+fun main() {
+    println(solution(arrayOf("Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan")).contentToString())
+}
