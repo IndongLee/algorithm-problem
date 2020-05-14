b5639adaebd2c51af6f51f1e8dd312a5ef907bf2
IndongLee <kundera@kakao.com>
2020-05-15 00:32:05 +0900

    Solve 괄호변환

--- Kotlin/src/Programmers/L2/L2_괄호변환.kt
+++ Kotlin/src/Programmers/L2/L2_괄호변환.kt
@@ -1,16 +1,43 @@
 package Programmers.L2
 
 private fun solution(p: String): String {
-    var answer = ""
-    return answer
+    return changeBracket(p)
 }
 
-tailrec fun bracket(b: String) {
-    if {
-        (b.isEmpty())
-    } return
+fun changeBracket(bracket: String): String {
+    return if (bracket.isEmpty()) {
+        bracket
+    } else {
+        val pivot = findPivot(bracket)
+        val isCorrect = bracket[0] != ')'
+
+        val u = bracket.take(pivot + 1)
+        val v = changeBracket(bracket.takeLast(bracket.lastIndex - pivot))
+
+        when(isCorrect) {
+            true -> u + v
+            false -> "($v)${u.drop(1).dropLast(1).asSequence().map{ if (it == '(') ')' else '(' }.joinToString("")}"
+        }
+    }
+
 }
+
+fun findPivot(bracket: String): Int {
+    var left = 0
+    var right = 0
+    for (i in bracket.indices) {
+        when (bracket[i]) {
+            '(' -> left++
+            ')' -> right++
+        }
+        if (left != 0 && right != 0 && left == right) {
+            return i
+        }
+    }
+    return bracket.length
+}
+
 
 fun main() {
-
+    println(solution("()))((()"))
 }
